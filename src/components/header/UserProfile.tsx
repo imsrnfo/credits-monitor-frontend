3a35bb25860a9f1a6f1aa5902bf9cbb314fe89f5
ignacio.silva <igmasiri@gmail.com>
2025-04-13 15:41:28 -0300

    show olny charts

---
+++ src/components/header/UserProfile.tsx
@@ -0,0 +1,9 @@
+export default function UserProfile() {
+  return (
+    <div className="flex items-center">
+      <span className="block font-medium text-theme-sm text-gray-700 dark:text-gray-400">
+        Musharof
+      </span>
+    </div>
+  );
+} 
