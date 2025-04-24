e897c857240c1c7872b1154975d29ecc601f1ab7
ignacio.silva <igmasiri@gmail.com>
2025-04-23 21:52:46 -0300

    Updates

--- src/pages/Dashboard/Home.tsx
+++
@@ -1,9 +0,0 @@
-export default function Home() {
-  return (
-    <div className="p-4">
-      <h1 className="text-2xl font-semibold text-gray-900 dark:text-white">
-        Dashboard
-      </h1>
-    </div>
-  );
-}
